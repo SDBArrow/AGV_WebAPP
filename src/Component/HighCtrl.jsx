4747a78fb39ab946365374c2b23045b288d8c354
YangTzuHung <j25889651556@gmail.com>
2022-04-15 13:47:14 +0800

    202204151347

--- src/Component/HighCtrl.jsx
+++ src/Component/HighCtrl.jsx
@@ -4,22 +4,20 @@ import RobotState from './RobotState';
 import Map from './Map_HighCtrl';
 import TodosList from "./TodosList_GoalSet"
 import Popup from './Popup';
+import Mode_Change from './Mode_Change';
 
 function HighCtrl() {
 
-  const [mode, setMode] = useState(true)
-  const [goal_name, setGoalName] = useState("")
   const [todos, setTodos] = useState([]);
   const [BT_GetGoalSet, setBT_GetGoalSet] = useState(false);
   const [ButtonPop, setButtonPop] = useState(false);
   const [inputValue, setInputValue] = useState("");
 
-
   function get_goalset() {
     setBT_GetGoalSet(!BT_GetGoalSet)
   }
 
-  function callPopup( msg ) {
+  function callPopup(msg) {
     setInputValue(msg)
     setButtonPop(true)
   }
@@ -29,22 +27,6 @@ function HighCtrl() {
 
     console.log(window.$sendGoal)
   }, [])
-
-  useEffect(() => {
-    localStorage.setItem("mode", mode);
-  }, [mode]);
-
-  useEffect(() => {
-    localStorage.setItem("goal_name", goal_name);
-  }, [goal_name]);
-
-  function change_mode() {
-    setMode(!mode)
-  }
-
-  const GetGoalName = (event) => {
-    setGoalName(event.target.value)
-  }
 
   //更新 新增的Goal
   useEffect(() => {
@@ -71,15 +53,13 @@ function HighCtrl() {
 
   return (
     <div>
-       <Popup trigger={ButtonPop} setButtonPop={setButtonPop} inputValue={inputValue} />
+      <Popup trigger={ButtonPop} setButtonPop={setButtonPop} inputValue={inputValue} />
       <p className="text-center text-5xl mt-5">AGV Set Page</p>
       <Connection />
       <div className="flex justify-evenly items-center flex-wrap-reverse ">
-
         <div className="w-96 bg-indigo-50 rounded-3xl py-20 select-none px-4 mt-5">
           <div className="bg-logo1 w-full h-32 bg-no-repeat bg-center bg-contain " />
-          <div className='h-2 mt-6' />
-          <div className='font-serif text-xl font-bold text-center'>AGV Goal 清單</div>
+          <div className='font-serif text-xl font-bold text-center mt-8'>AGV Goal 清單</div>
           <TodosList todos={todos} setTodos={setTodos} />
           <div className='grid justify-items-center mt-8 '>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-9 w-9 cursor-pointer fill-white hover:fill-red-300" onClick={get_goalset} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
@@ -87,35 +67,11 @@ function HighCtrl() {
             </svg>
           </div>
         </div>
-
         <div>
-
-          <div className=' bg-indigo-50 rounded-3xl py-10 select-none px-4 mt-5 grid justify-items-center'>
-            <div className='font-serif text-xl font-bold'>模式控制</div>
-            <div className='flex h-10 w-20 rounded-full mt-5 bg-gray-300 relative'>
-              <label htmlFor="check">
-                <input type="checkbox" id="check" className="sr-only peer" />
-                <span className='w-2/5 h-4/5 bg-red-500 absolute rounded-full left-1 top-1 peer-checked:bg-blue-500 peer-checked:left-11 transition-all duration-500' onClick={change_mode} >
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-1 mt-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d={`${mode ? "M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" : "M12 19l9 2-9-18-9 18 9-2zm0 0v-8"}`} />
-                  </svg>
-                </span>
-              </label>
-            </div>
-            <div className="mt-5 font-serif text-center text-blue-600">Mode：{mode ? "新增模式" : "導航模式"}</div>
-
-            <div className={`bg-white rounded-2xl w-40 py-3 mt-5 ${mode ? "" : "hidden"}`}>
-              <label className="ml-4">新增的目標名稱：</label>
-              <input className="h-7 w-32 mt-1 ml-4 pl-1" placeholder="座位1" onChange={GetGoalName} />
-            </div>
-          </div>
-
+          <Mode_Change />
           <RobotState />
-
         </div>
-
         <Map />
-
       </div>
     </div >
   );
---
+++ src/Component/Mode_Change.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+
+function ModeChange() {
+
+  const [mode, setMode] = useState(true)
+  const [goal_name, setGoalName] = useState("")
+
+  useEffect(() => {
+    localStorage.setItem("mode", mode);
+  }, [mode]);
+
+  useEffect(() => {
+    localStorage.setItem("goal_name", goal_name);
+  }, [goal_name]);
+
+  function change_mode() {
+    setMode(!mode)
+  }
+
+  const GetGoalName = (event) => {
+    setGoalName(event.target.value)
+  }
+
+  return (
+          <div className=' bg-indigo-50 rounded-3xl py-10 select-none px-4 mt-5 grid justify-items-center'>
+            <div className='font-serif text-xl font-bold'>模式控制</div>
+            <div className='flex h-10 w-20 rounded-full mt-5 bg-gray-300 relative'>
+              <label htmlFor="check">
+                <input type="checkbox" id="check" className="sr-only peer" />
+                <span className='w-2/5 h-4/5 bg-red-500 absolute rounded-full left-1 top-1 peer-checked:bg-blue-500 peer-checked:left-11 transition-all duration-500' onClick={change_mode} >
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-1 mt-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d={`${mode ? "M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" : "M12 19l9 2-9-18-9 18 9-2zm0 0v-8"}`} />
+                  </svg>
+                </span>
+              </label>
+            </div>
+            <div className="mt-5 font-serif text-center text-blue-600">Mode：{mode ? "新增模式" : "導航模式"}</div>
+            <div className={`bg-white rounded-2xl w-40 py-3 mt-5 ${mode ? "" : "hidden"}`}>
+              <label className="ml-4">新增的目標名稱：</label>
+              <input className="h-7 w-32 mt-1 ml-4 pl-1" placeholder="座位1" onChange={GetGoalName} />
+            </div>
+          </div>
+  );
+}
+
+export default ModeChange;
